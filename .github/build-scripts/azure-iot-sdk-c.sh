337c614ad014e2b8a8b95011f483e88e87130901
Mingjie Shen <shen497@purdue.edu>
2023-03-28 19:20:36 +0000

    Add build-script

---
+++ .github/build-scripts/azure-iot-sdk-c.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash#
+
+sudo apt-get update
+sudo apt-get install -y git cmake build-essential curl libcurl4-openssl-dev libssl-dev uuid-dev ca-certificates
+mkdir cmake
+cd cmake
+cmake ..
+cmake --build . -- -j$(nproc)
