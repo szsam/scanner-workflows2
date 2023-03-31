0475842ca3d34fa30954ceb97d31c4f33f8e1d08
Mingjie Shen <shen497@purdue.edu>
2023-03-31 03:25:17 +0000

    Add workflow for azure-rtos/threadx

--- .github/build-scripts/azure-rtos_threadx.sh
+++ .github/build-scripts/azure-rtos_threadx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install ninja
+sudo apt-get update -y 
+sudo apt-get install -y ninja-build
 cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m4.cmake .
 cmake --build ./build
