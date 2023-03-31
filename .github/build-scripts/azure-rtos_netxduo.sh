677cff89a0a25667e98b53ee7c76acba69aa32be
Mingjie Shen <shen497@purdue.edu>
2023-03-31 06:55:24 +0000

    Update workflows

--- .github/build-scripts/azure-rtos_netxduo.sh
+++ .github/build-scripts/azure-rtos_netxduo.sh
@@ -3,11 +3,11 @@
 sudo apt-get update -y
 sudo apt-get install -y ninja-build
 
-git clone https://github.com/azure-rtos/threadx.git
-cd threadx
-cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m4.cmake .
-cmake --build ./build
-cd ..
+#git clone https://github.com/azure-rtos/threadx.git
+#cd threadx
+#cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m4.cmake .
+#cmake --build ./build
+#cd ..
 
 cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m4.cmake .
 cmake --build ./build
