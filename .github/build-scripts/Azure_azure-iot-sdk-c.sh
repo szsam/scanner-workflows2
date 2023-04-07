a4f0201b63c93d6d53fa8c4caf5c69ca8bae8f3b
Mingjie Shen <shen497@purdue.edu>
2023-04-07 04:41:24 +0000

    Update

--- .github/build-scripts/Azure_azure-iot-sdk-c.sh
+++ .github/build-scripts/Azure_azure-iot-sdk-c.sh
@@ -2,7 +2,7 @@
 
 # https://github.com/Azure/azure-iot-sdk-c/blob/main/doc/devbox_setup.md#linux
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y git cmake build-essential curl libcurl4-openssl-dev libssl-dev uuid-dev ca-certificates
 mkdir cmake
 cd cmake
