f53687a8e6cbab3d1d32c367ad467eee143972a6
Mingjie Shen <shen497@purdue.edu>
2023-04-08 19:54:00 +0000

    Update micropython

--- .github/build-scripts/micropython_micropython.sh
+++ .github/build-scripts/micropython_micropython.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update -y
 sudo apt-get install -y libffi-dev # for header ffi.h
-source tools/ci.sh && ci_unix_standard_build && ci_mpy_format_setup
+source tools/ci.sh && ci_unix_standard_build
