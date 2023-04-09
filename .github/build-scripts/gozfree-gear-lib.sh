f80fcdeeee763bd5a9e52583c9e1896e6366f89c
Mingjie Shen <shen497@purdue.edu>
2023-04-09 21:30:10 +0000

    Add gozfree/gear-lib

--- .github/build-scripts/gozfree-gear-lib.sh
+++ .github/build-scripts/gozfree-gear-lib.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-#mkdir build_linux
-#cd build_linux
-#cmake ../
-#make -j$(nproc)
-
 sudo apt-get update -y
 sudo apt-get install -y liblua5.2-dev libjansson-dev libpulse-dev libx264-dev libavcodec-dev libavformat-dev libavutil-dev libuvc-dev protobuf-compiler
-./build.sh
+
+mkdir build_linux
+cd build_linux
+cmake ../
+make -j$(nproc)
+
+#./build.sh
