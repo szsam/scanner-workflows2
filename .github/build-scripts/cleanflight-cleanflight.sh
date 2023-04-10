6f75a5ce5612b1b0e75ca2bd938aa839952125f6
Mingjie Shen <shen497@purdue.edu>
2023-04-10 00:21:32 +0000

    Add cleanflight/cleanflight

--- .github/build-scripts/cleanflight-cleanflight.sh
+++ .github/build-scripts/cleanflight-cleanflight.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export GOAL=all #test-all
+#export GOAL=all #test-all
 sudo apt-get update -y
 sudo apt-get install -y libc6-i386 libblocksruntime-dev #lcov
 make arm_sdk_install
 make arm_sdk_version
-./.travis.sh
+TARGET=targets-f3 ./.travis.sh
