12c860f17abc80f820741762022e5f03ed91d70f
Mingjie Shen <shen497@purdue.edu>
2023-04-10 05:00:38 +0000

    Add InfiniTime

--- .github/build-scripts/InfiniTimeOrg-InfiniTime.sh
+++ .github/build-scripts/InfiniTimeOrg-InfiniTime.sh
@@ -3,6 +3,7 @@
 set -e
 
 sudo apt-get install -y python3-venv python3-pip
+npm install lv_font_conv
 
 wget --no-verbose 'https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v15.x.x/nRF5_SDK_15.3.0_59ac345.zip'
 unzip -q nRF5_SDK_15.3.0_59ac345
@@ -12,7 +13,6 @@ source .venv/bin/activate
 python3 -m pip install wheel
 python3 -m pip install -r tools/mcuboot/requirements.txt
 
-ls -al
 mkdir build
 cd build
 cmake .. -DNRF5_SDK_PATH=$(realpath nRF5_SDK_15.3.0_59ac345) \
