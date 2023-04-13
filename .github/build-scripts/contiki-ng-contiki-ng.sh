3812a9944840073bb6700860bc648179b8090a7b
Mingjie Shen <shen497@purdue.edu>
2023-04-13 02:08:13 -0400

    Add contiki-ng/contiki-ng

--- .github/build-scripts/contiki-ng-contiki-ng.sh
+++ .github/build-scripts/contiki-ng-contiki-ng.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-make -C examples/hello-world
-make -C examples/mqtt-client
+CNT_SUCCESS=0
+CNT_ALL=0
+
+for DIR in $(find examples/ -type d -exec test -f '{}'/Makefile \; -print -prune); do
+  echo $DIR && ((CNT_ALL++))
+  make -C $DIR -j$(nproc) && ((CNT_SUCCESS++))
+  make -C $DIR clean
+done
+
+echo  Succeeded in compiling $CNT_SUCCESS/$CNT_ALL apps
