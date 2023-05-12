5820d8add3dcc80e8346552ad0d3d9e9667f0596
Brian <bayuan@purdue.edu>
2023-05-12 15:14:06 -0400

    build: splinterdb

---
+++ .github/build-scripts/vmware-splinterdb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export COMPILER=gcc
+sudo apt update -y
+sudo apt-get install -y libaio-dev libconfig-dev libxxhash-dev $COMPILER
+export CC=$COMPILER
+export LD=$COMPILER
+make
+make run-tests
+sudo make install
