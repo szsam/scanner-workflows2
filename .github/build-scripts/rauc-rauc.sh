dd7f2bf0526b001536978dee875d00d64857770f
Brian <bayuan@purdue.edu>
2023-05-31 13:10:01 -0400

    build: rauc fix"

--- .github/build-scripts/rauc-rauc.sh
+++ .github/build-scripts/rauc-rauc.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y build-essential meson automake libtool libdbus-1-dev libglib2.0-dev libcurl3-dev libssl-dev
-sudo apt-get install -y libnl-genl-3-dev
-sudo apt-get install -y libjson-glib-dev
-meson setup build
-meson compile -C build
+#sudo apt-get install -y build-essential meson automake libtool libdbus-1-dev libglib2.0-dev libcurl3-dev libssl-dev
+#sudo apt-get install -y libnl-genl-3-dev
+#sudo apt-get install -y libjson-glib-dev
+#meson setup build
+#meson compile -C build
+
+./autogen.sh
+./configure
+make
