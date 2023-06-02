94b84c5b7e3fbdf8762cd87c2bda23f996a926ba
Brian <bayuan@purdue.edu>
2023-06-01 22:25:16 -0400

    Build interrutp"

--- .github/build-scripts/memfault-interrupt.sh
+++ .github/build-scripts/memfault-interrupt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
-bundle install
+#pip install -r requirements.txt
+#bundle install
 # bundle exec jekyll serve --drafts --incremental --livereload --open-url
+
+./build.sh
