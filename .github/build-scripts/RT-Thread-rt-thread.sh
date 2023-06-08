f01afe5c87228b606ede0e992340bb4658d526ad
Mingjie Shen <shen497@purdue.edu>
2023-06-08 14:36:03 -0400

    Update rt-thread

--- .github/build-scripts/RT-Thread-rt-thread.sh
+++ .github/build-scripts/RT-Thread-rt-thread.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 export RTT_EXEC_PATH="$(dirname $(which arm-none-eabi-gcc))"
-cd bsp/stm32/stm32f103-atk-nano
-scons -j$(nproc)
+cd bsp
+
+scons -j$(nproc) -C qemu-vexpress-a9
+#scons -j$(nproc) -C stm32/stm32f103-atk-nano
+scons -j$(nproc) -C stm32/stm32l4r5-st-nucleo
+scons -j$(nproc) -C stm32/stm32f207-st-nucleo
+scons -j$(nproc) -C stm32/stm32f746-st-disco
+scons -j$(nproc) -C lpc55sxx/lpc55s69_nxp_evk
+scons -j$(nproc) -C gd32/arm/gd32103c-eval
+scons -j$(nproc) -C nrf5x/nrf51822
