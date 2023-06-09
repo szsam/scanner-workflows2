0714b16ac759561e85a9786fd582f6a4879a281f
Mingjie Shen <shen497@purdue.edu>
2023-06-09 00:41:00 -0400

    Update nuttx

--- .github/build-scripts/apache-nuttx.sh
+++ .github/build-scripts/apache-nuttx.sh
@@ -4,10 +4,21 @@ sudo apt-get install kconfig-frontends
 
 git clone https://github.com/apache/nuttx-apps.git ../apps
 
-for cfg in $(./tools/configure.sh  -L | grep 'stm32f103-minimum'); do
-  make distclean
+TOPDIR=.
+DUMPCFGS=""
+configlist=`find ${TOPDIR}/boards/arm -name defconfig`
+for defconfig in ${configlist}; do
+  config=`dirname ${defconfig} | sed -e "s,${TOPDIR}/boards/,,g"`
+  boardname=`echo ${config} | cut -d'/' -f3`
+  configname=`echo ${config} | cut -d'/' -f5`
+  DUMPCFGS="${DUMPCFGS} ${boardname}:${configname}"
+done
+
+for cfg in $(echo "$DUMPCFGS" | sed 's, ,\n,g' | sort -t ':' -k2 -u); do
+  echo "Compiling $cfg"
   ./tools/configure.sh $cfg
   make CROSSDEV=arm-none-eabi- -j$(nproc)
+  make distclean
 done
 
 rm -rf ../apps
