877aaf091c85adeb4aeb12d593c88ed44ff59957
Mingjie Shen <shen497@purdue.edu>
2023-06-09 14:40:59 -0400

    Update TrampolineRTOS

--- .github/build-scripts/TrampolineRTOS-trampoline.sh
+++ .github/build-scripts/TrampolineRTOS-trampoline.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 cd goil/makefile-unix/
-./build.py
+./build.py release
+cd -
+GOIL="$(realpath goil/makefile-unix/goil) "
+export GOIL_TEMPLATES="$(realpath goil/templates)"
 
+# Not needed if we do not actually run the apps
+#cd viper && make && cd -
+
+cd examples/posix/one_task
+$GOIL --target=posix/linux one_task.oil
+./make.py
