d483a5c5d01c6c659f3ceb446795fd0bdadad996
Mingjie Shen <shen497@purdue.edu>
2023-06-12 15:27:07 -0400

    Update mynewt-core

--- .github/build-scripts/apache-mynewt-core.sh
+++ .github/build-scripts/apache-mynewt-core.sh
@@ -9,14 +9,18 @@
 sudo apt-get update -y
 sudo apt-get install -y newt gcc-multilib
 
-# .github/workflows/build_blinky.yml
+# .github/workflows/
 cp .github/project.yml project.yml
 newt upgrade --shallow=1
-rm -rf $RUNNER_TEMP/blinky
-git clone https://github.com/apache/mynewt-blinky.git $RUNNER_TEMP/blinky
-cp -r $RUNNER_TEMP/blinky/apps/blinky/ apps/blinky
-bash .github/test_build_blinky.sh || true
+
+# .github/workflows/build_blinky.yml
+#rm -rf $RUNNER_TEMP/blinky
+#git clone https://github.com/apache/mynewt-blinky.git $RUNNER_TEMP/blinky
+#cp -r $RUNNER_TEMP/blinky/apps/blinky/ apps/blinky
+#bash .github/test_build_blinky.sh || true
 
 ## .github/workflows/build_targets.yml
-#cp -r .github/targets ci_targets
-#ls ci_targets | xargs -n1 sh -c 'echo "Testing $0"; newt build -q $0'
+cp -r .github/targets ci_targets
+ls ci_targets | xargs -n1 sh -c 'echo "Testing $0"; newt build -q $0'
+
+exit 0
