20e9983b0392d8411f88c6848b2af0197c2a9f95
Mingjie Shen <shen497@purdue.edu>
2023-06-28 00:15:29 -0400

    Update freertos

--- .github/build-scripts/FreeRTOS-FreeRTOS.sh
+++ .github/build-scripts/FreeRTOS-FreeRTOS.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-#cd FreeRTOS/Demo/Posix_GCC/
-#make -j$(nproc)
+sudo apt-get -y update
+sudo apt-get -y install build-essential cmake git ninja-build python3-minimal
 
-# .github/workflows/kernel-unit-tests.yml
-sudo apt-get install -y lcov cflow ruby doxygen build-essential unifdef
+make -j$(nproc) -C FreeRTOS/Demo/Posix_GCC
+make            -C FreeRTOS/Demo/Posix_GCC clean
+make -j$(nproc) -C FreeRTOS/Demo/Posix_GCC USER_DEMO=BLINKY_DEMO
 
-make -C FreeRTOS/Test/CMock clean
-make -C FreeRTOS/Test/CMock run_col_formatted -j$(nproc)
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_MPU_M3_MPS2_QEMU_GCC
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_LM3S102_GCC
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC
+make            -C FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC clean
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC FULL_DEMO=1
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_LM3S811_GCC
+# FreeRTOS/Demo/ThirdParty/Community-Supported/CORTEX_M0+_RP2040
+make -j$(nproc) -C FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/build/gcc
