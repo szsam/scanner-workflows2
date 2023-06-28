8188cc8c7ab0c7c61363351d5b91b7db75bd65da
Mingjie Shen <shen497@purdue.edu>
2023-06-27 23:50:59 -0400

    Update freertos

--- .github/build-scripts/FreeRTOS-FreeRTOS.sh
+++ .github/build-scripts/FreeRTOS-FreeRTOS.sh
@@ -7,4 +7,4 @@
 sudo apt-get install -y lcov cflow ruby doxygen build-essential unifdef
 
 make -C FreeRTOS/Test/CMock clean
-make -C FreeRTOS/Test/CMock ENABLE_SANITIZER=1 run_col_formatted
+make -C FreeRTOS/Test/CMock run_col_formatted -j$(nproc)
