89d49a933f5195b154f517993c956fb52df9d00e
Mingjie Shen <shen497@purdue.edu>
2023-06-27 23:40:54 -0400

    Update freertos

--- .github/build-scripts/FreeRTOS-FreeRTOS.sh
+++ .github/build-scripts/FreeRTOS-FreeRTOS.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-cd FreeRTOS/Demo/Posix_GCC/
-make -j$(nproc)
+#cd FreeRTOS/Demo/Posix_GCC/
+#make -j$(nproc)
+
+# .github/workflows/kernel-unit-tests.yml
+sudo apt-get install -y lcov cflow ruby doxygen build-essential unifdef
+
+make -C FreeRTOS/Test/CMock clean
+make -C FreeRTOS/Test/CMock ENABLE_SANITIZER=1 run_col_formatted
