64e55bf9f6dc3a0d4b5a7294deaa29a72d907270
Mingjie Shen <shen497@purdue.edu>
2023-06-28 00:36:04 -0400

    Update freertos

--- .github/build-scripts/FreeRTOS-FreeRTOS.sh
+++ .github/build-scripts/FreeRTOS-FreeRTOS.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt-get -y update
-sudo apt-get -y install build-essential cmake git ninja-build python3-minimal
+sudo apt-get -y install build-essential cmake git ninja-build python3-minimal libpcap-dev
 
+# FreeRTOS demos
 make -j$(nproc) -C FreeRTOS/Demo/Posix_GCC
 make            -C FreeRTOS/Demo/Posix_GCC clean
 make -j$(nproc) -C FreeRTOS/Demo/Posix_GCC USER_DEMO=BLINKY_DEMO
@@ -15,3 +16,7 @@ make -j$(nproc) -C FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC FULL_DEMO=1
 make -j$(nproc) -C FreeRTOS/Demo/CORTEX_LM3S811_GCC
 # FreeRTOS/Demo/ThirdParty/Community-Supported/CORTEX_M0+_RP2040
 make -j$(nproc) -C FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/build/gcc
+
+# FreeRTOS_Plus demos
+make -j$(nproc) -C FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Posix
+#make -j$(nproc) -C FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Qemu_mps2 # compile error
