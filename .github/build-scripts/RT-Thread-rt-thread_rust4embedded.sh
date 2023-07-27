e1700f8bc1e36e3eb4387af0b2ac7b04a8074452
Shank <shashanksharma21@gmail.com>
2023-07-27 11:40:21 -0400

    [rust4embedded] update call to bear

--- .github/build-scripts/RT-Thread-rt-thread_rust4embedded.sh
+++ .github/build-scripts/RT-Thread-rt-thread_rust4embedded.sh
@@ -12,7 +12,7 @@ cd bsp
 # #scons -j$(nproc) -C gd32/arm/gd32103c-eval #compile error
 # scons -j$(nproc) -C n32/n32g43xcl-stb
 # scons -j$(nproc) -C apm32/apm32f103xe-minibroard
-bear scons -j$(nproc) -C nrf5x/nrf51822
+bear -- scons -j$(nproc) -C nrf5x/nrf51822
 # scons -j$(nproc) -C Infineon/psoc6-cy8ckit-062S2-43012
 # scons -j$(nproc) -C fujitsu/mb9x/mb9bf500r
 # #scons -j$(nproc) -C raspberry-pico # got stuck when building db
