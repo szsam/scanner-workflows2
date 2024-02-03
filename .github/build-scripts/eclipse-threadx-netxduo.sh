0d3d100022d9e1c41c59566ddc008aaa89cc4a84
Mingjie Shen <shen497@purdue.edu>
2024-02-02 20:16:25 -0500

    Add eclipse-threadx/netxduo

---
+++ .github/build-scripts/eclipse-threadx-netxduo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/eclipse-threadx/threadx.git
+# Insert "add_subdirectory(threadx)" before "target_link_libraries(...azrtos::threadx)"
+sed -i '/target_link_libraries(.*threadx/ i\add_subdirectory(threadx)'
+cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m4.cmake -DNXD_ENABLE_FILE_SERVERS=OFF .
+cmake --build ./build
