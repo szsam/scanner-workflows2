516fbd1e9d95db0b248e33aa0130865ac300e408
Mingjie Shen <shen497@purdue.edu>
2024-02-07 18:19:54 -0500

    Delete outdated

--- .github/build-scripts/azure-rtos_getting-started.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update -y
-sudo apt-get install -y ninja-build
-
-cd Microchip/ATSAME54-XPRO
-cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE="../../cmake/arm-gcc-cortex-m4.cmake"
-cmake --build build
